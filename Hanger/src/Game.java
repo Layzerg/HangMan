8bc67f736cc2095733f76e1894a2d92620a42786
Layzerg <layzerg44@gmail.com>
2023-11-20 11:03:29 +0300

    global rework

---
+++ Hanger/src/Game.java
@@ -0,0 +1,107 @@
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.TreeSet;
+
+
+public class Game {
+    public char[] currentState;
+    public char[] enigma;
+    int status = 0;
+    TreeSet<Character> guesses = new TreeSet<>();
+    PreparingTheRiddle preparingTheRiddle = new PreparingTheRiddle();
+    TextMessages textMessages = new TextMessages();
+    UserInput userInput = new UserInput();
+    Drawings drawings = new Drawings();
+
+
+    public Game() throws IOException {
+        textMessages.greetingMessage();
+        gameOn();
+    //    System.out.println(Arrays.toString(enigma));
+    //    System.out.println(Arrays.toString(currentState));
+        while(true) {
+            textMessages.printCurrentState(currentState);
+            char answer = userInput.userInput(guesses);
+            if(checkAnswer(answer)){
+               rightAnswer(answer);
+            }
+            else{
+                wrongAnswer(answer);
+            }
+            if(Arrays.equals(enigma, currentState)){
+                youWon();
+            }
+            if(status== drawings.getDrawings().length-1){
+                youLost();
+            }
+        }
+    }
+
+    public boolean checkAnswer(char answer){
+        boolean isCorrect = false;
+        for(char ch : enigma){
+            if(answer == ch){
+                isCorrect= true;
+                break;
+            }
+        }
+        return isCorrect;
+    }
+
+
+
+    private boolean gameOn() throws IOException {
+        textMessages.startGame();
+        char yn = userInput.userYNInput();
+        if(yn=='y'){
+            char[][] enigmaAndCurrentState = preparingTheRiddle.preparingGame();
+            enigma = enigmaAndCurrentState[0];
+            currentState = enigmaAndCurrentState[1];
+            status = 0;
+            textMessages.timeToGuess();
+            return true;
+        }
+        else{
+            textMessages.goodByeMessage();
+            userInput.br.close();
+            System.exit(1);
+            return false;
+        }
+    }
+    private void wrongAnswer(char answer){
+        status++;
+        textMessages.wrongAnswerMessage();
+        guesses.add(answer);
+        textMessages.printGuesses(guesses);
+        textMessages.printCurrentState(currentState);
+        drawings.printDrawing(status);
+    }
+    private void rightAnswer(char answer){
+        for(int i= 0; i < currentState.length; i++){
+            if(enigma[i] == answer){
+                currentState[i] = answer;
+            }
+        }
+        textMessages.rightAnswerMessage();
+        guesses.add(answer);
+        textMessages.printGuesses(guesses);
+        textMessages.printCurrentState(currentState);
+        drawings.printDrawing(status);
+    }
+    private void youWon() throws IOException {
+        textMessages.youWonMessage();
+        guesses.clear();
+        gameOn();
+    }
+    private void youLost() throws IOException {
+        textMessages.gameOver();
+        System.out.print("                             Word was: ");
+        for (char ch : enigma) {
+            System.out.print(ch);
+        }
+        System.out.println();
+        guesses.clear();
+        gameOn();
+    }
+}
---
+++ Hanger/src/UserInput.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.TreeSet;
+
+public class UserInput {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public UserInput() {
+
+    }
+
+    public char userInput(TreeSet<Character> set) {
+        char resInput = '0';
+        boolean validInput = false;
+        while(!validInput){
+            String input = getUserInput();
+            if(!checkInputForError(input, set)){
+                System.out.println("Try again :)");
+            }
+            else{
+                resInput = input.charAt(0);
+                validInput = true;
+            }
+        }
+        return resInput;
+    }
+
+    public  char userYNInput(){
+        char resInput = '0';
+        boolean validInput = false;
+        while(!validInput){
+            String input = getUserInput();
+            if(!checkInputForYN(input)){
+                System.out.println("Try again :)");
+            }
+            else{
+                resInput = input.charAt(0);
+                validInput = true;
+            }
+        }
+        return resInput;
+    }
+
+    private boolean checkInputForError(String s, TreeSet<Character> set) {
+        if(s.length() == 0){
+            return false;
+        }
+        char res = s.charAt(0);
+        boolean approoved = false;
+        if (s.length() > 1) {
+            System.out.println("You need to choose one letter");
+            return approoved;
+        } else if (Character.isDigit(res)) {
+            System.out.println("Letter not the number... Now you need to choose one letter");
+            return approoved;
+        } else if (set.contains(res)) {
+            System.out.println("But that's the same letter...");
+            return approoved;
+        } else if (Character.isAlphabetic(s.charAt(0))) {
+            approoved = true;
+            return approoved;
+        }
+        return approoved;
+    }
+    private boolean checkInputForYN(String s){
+        if(s.length() == 0){
+            System.out.println("There gotta be some input");
+            return false;
+        }
+        char res = s.charAt(0);
+        boolean approoved = false;
+        if (s.length() > 1) {
+            System.out.println("You need to choose one letter, Y or N");
+            return approoved;
+        } else if (Character.isDigit(res)) {
+            System.out.println("Letter not the number... Now you need to choose one letter");
+            return approoved;
+        }  else if (!Character.isAlphabetic(s.charAt(0))) {
+            return approoved;
+        } else if (res == 'y' || res == 'n') {
+            approoved = true;
+            return approoved;
+        }
+        return approoved;
+    }
+    private String getUserInput(){
+        String input = null;
+        try {
+            input = br.readLine().toLowerCase();
+   //         br.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return input;
+    }
+}
